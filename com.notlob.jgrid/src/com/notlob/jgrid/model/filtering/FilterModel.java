7171277ec0e4bf28cc0627af763043929260b925
Stef Bolton <sbolton@gresham-computing.com>
2015-04-21 06:47:13 +0100

    Fixed a bug in filtering, group rows should always be shown

--- com.notlob.jgrid/src/com/notlob/jgrid/model/filtering/FilterModel.java
+++ com.notlob.jgrid/src/com/notlob/jgrid/model/filtering/FilterModel.java
@@ -79,9 +79,9 @@ public class FilterModel<T> {
 		for (final Filter<T> filter : filters) {
 			boolean matches = false;
 			
-			if (filter.isShowWholeGroup() && gridModel.isGroupRow(row)) {
+			if ((filter.isShowWholeGroup() && gridModel.isGroupRow(row)) || gridModel.isParentRow(row)) {
 				//
-				// If anything in the group match then this row should be shown.
+				// If anything in the group match then this row should be shown OR if it's a parent row, always show it.
 				//
 				for (Row<T> relative : gridModel.getWholeGroup(row)) {
 					if (filter.matches(relative)) {
